712694b009352341f576df6e009a7138c422b608
Pavel Belousov <signorpao@gmail.com>
2024-06-21 21:24:43 +0300

    ...mobile menu engine is ready.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
 const burgerButton = document.getElementById("burger"),
-  mobileMenu = document.querySelector(".mobile__menu");
+  mobileMenu = document.querySelector(".mobile__menu"),
+  body = document.querySelector("body"),
+  overlay = document.querySelector(".overlay__body");
 
 // mobile menu show / close
 burgerButton.addEventListener("click", () => {
   mobileMenu.classList.toggle("active");
+  body.classList.toggle("lock");
+  overlay.classList.toggle("active");
 
   // change icons
   mobileMenu.classList.contains("active")
